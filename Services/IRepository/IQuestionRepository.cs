ffb677ca8510e49cdf72508e3f6c216450c29e70
anhpdhe161578 <anhpdhe161578@fpt.edu.vn>
2024-11-29 18:14:44 +0700

    list and detail question

---
+++ Services/IRepository/IQuestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using Quizpractice.Models;
+
+namespace Quizpractice.Services.IRepository
+{
+    public interface IQuestionRepository : IBaseRepository<Question>
+    {
+       
+        Task<IEnumerable<Question>> GetQuestionsWithDetailsAsync();
+        Task<Question> GetQuestionWithAnswersAsync(int questionId);
+    }
+
+    public interface IAnswerRepository : IBaseRepository<Answer>
+    {
+    }
+}
+
---
+++ Services/Repository/QuestionRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quizpractice.Models;
+using Quizpractice.Services.IRepository;
+
+namespace Quizpractice.Services.Repository
+{
+    public class QuestionRepository : BaseRepository<Question>, IQuestionRepository
+    {
+        private readonly SWP391_DBContext _context;
+        public QuestionRepository(SWP391_DBContext context) : base(context)
+        {
+            _context = context;
+        }
+        public async Task<Question> GetQuestionWithAnswersAsync(int questionId)
+        {
+            return await _context.Questions
+                .Include(q => q.Answers) 
+                .Include(q => q.Subject) 
+                .Include(q => q.Lesson) 
+                .Include(q => q.Topic) 
+                .FirstOrDefaultAsync(q => q.QuestionId == questionId);
+        }
+
+        public async Task<IEnumerable<Question>> GetQuestionsWithDetailsAsync()
+        {
+            return await _context.Questions
+                .Include(q => q.Subject)
+                .Include(q => q.Lesson)
+                .Include(q => q.Topic)
+                .ToListAsync();
+        }
+
+
+        public class AnswerRepository : BaseRepository<Answer>, IAnswerRepository
+        {
+            public AnswerRepository(SWP391_DBContext context) : base(context)
+            {
+            }
+        }
+    }
+
+}
