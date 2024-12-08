395abceae30a2c7c573e59321f97da64ef146416
ducbdhe161544 <ducbdhe161544@fpt.edu.vn>
2024-12-09 01:09:48 +0700

    blog

---
+++ Pages/Blogs/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Quizpractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuizletApp.Pages.Blogs
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SWP391_DBContext _context;
+
+        public IndexModel(SWP391_DBContext context)
+        {
+            _context = context;
+        }
+
+        public List<Blog> Blogs { get; set; }
+        public string SearchQuery { get; set; }
+
+        public void OnGet(string searchQuery)
+        {
+            SearchQuery = searchQuery ?? string.Empty;
+
+            if (string.IsNullOrEmpty(SearchQuery))
+            {
+                Blogs = _context.Blogs.ToList();
+            }
+            else
+            {
+                Blogs = _context.Blogs
+                    .Where(b => b.BlogName != null && b.BlogName.ToLower().Contains(SearchQuery.ToLower()))
+                    .ToList();
+            }
+        }
+    }
+}
