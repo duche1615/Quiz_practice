359abab9f5adb69edcc0c7ca2499ea113cf5dd37
ducbdhe161544 <ducbdhe161544@fpt.edu.vn>
2024-12-13 02:42:41 +0700

    List lession

---
+++ Pages/ListLession/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Quizpractice.Models;
+
+namespace Quizpractice.Pages.ListLession
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SWP391_DBContext _context;
+
+        public IndexModel(SWP391_DBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Lession> Lessions { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            // Lấy tất cả các bài học, kèm theo thông tin Subject và Chapter
+            Lessions = await _context.Lessions
+                .Include(l => l.Chapter) // Load thông tin Chapter
+                .Include(l => l.Sub) // Load thông tin Subject
+                .ToListAsync();
+        }
+    }
+}
